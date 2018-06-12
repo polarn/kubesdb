0b6ca171bb644efa51234bb3cccc6fdebd8e5441
Marcus Johansson <polarn@gmail.com>
2018-06-12 06:22:39 +0200

    added debug output and check if db exists

--- modules/database.py
+++ modules/database.py
@@ -14,20 +14,38 @@ class Database:
     def create_database(self, database, username, password):
         logging.info("Creating database: %s" % (database))
 
+        self.check_if_database_exists(database)
+
         try:
             with self.connection.cursor() as cursor:
                 sql_db = "CREATE DATABASE IF NOT EXISTS `%s`" % (database)
                 cursor.execute(sql_db)
+                logging.info(sql_db)
 
                 sql_user = "CREATE USER IF NOT EXISTS '%s'@'%%' IDENTIFIED BY '%s'" % (username, password)
                 cursor.execute(sql_user)
+                logging.info(sql_user)
 
                 sql_grant = "GRANT ALL PRIVILEGES ON `%s`.* TO '%s'@'%%'" % (database, username)
                 cursor.execute(sql_grant)
+                logging.info(sql_grant)
 
             self.connection.commit()
+            logging.info("committed")
         except:
             raise
+
+    def check_if_database_exists(self,database):
+        try:
+            with self.connection.cursor() as cursor:
+                sql = "SHOW DATABASES LIKE '%s'" % (database)
+                rows_returned = cursor.execute(sql)
+                cursor.fetchall()
+                if (rows_returned > 0):
+                    return True
+        except:
+            raise
+        return False
 
     def close(self):
         self.connection.close()
